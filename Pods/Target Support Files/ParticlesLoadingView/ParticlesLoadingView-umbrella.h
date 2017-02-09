409f3e80b5dd27b710276e4c4ede1bbdbdca2b39
Reginaldo <reginaldopaiva@hotmail.com.br>
2017-02-09 19:20:39 -0200

    gjfghfj

--- Pods/Target Support Files/ParticlesLoadingView/ParticlesLoadingView-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double ParticlesLoadingViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char ParticlesLoadingViewVersionString[];
-
